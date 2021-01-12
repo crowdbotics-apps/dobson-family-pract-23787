c9073323bbc39393b6d5a257d13f8b95f786beea
Crowdbotics <team@crowdbotics.com>
2021-01-12 14:18:02 +0000

    Update models

--- backend/dobson_family_pract_23787/wsgi.py
+++ backend/dobson_family_pract_23787/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dobson_family_pract_23787.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dobson_family_pract_23787.settings")
 
 application = get_wsgi_application()
